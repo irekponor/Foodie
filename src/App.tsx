155d590a607f05aed528032982ea1e83972a3f6e
emmanuel <emmanuelirekponor86@gmail.com>
2024-10-02 16:47:18 +0100

    Hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import MobNav from "./components/MobNav";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <MobNav />
     </>
   );
 }
--- src/components/MobNav.tsx
+++ src/components/MobNav.tsx
@@ -1,32 +1,26 @@
 import { AiOutlineShoppingCart, AiOutlineUser } from "react-icons/ai";
 import { BsSearch } from "react-icons/bs";
+import { IoIosMenu } from "react-icons/io";
 import Badge from "./Badge";
 
-const Navbar = () => {
+const MobNav = () => {
   return (
     <div className="sticky top-0 bg-white z-10">
-      <div className="container hidden lg:block">
-        <div className="flex justify-between items-center p-8">
-          <h1 className="text-3xl font-medium">Logo</h1>
-          <div className="relative w-full max-w-[500px]">
-            <input
-              className="bg-[#f5f5f5] border-none outline-none px-6 py-3 rounded-[30px] w-full"
-              type="text"
-              placeholder="Search..."
-            />
-            <BsSearch
-              className="top-0 absolute right-0 mt-4 mr-5 text-gray-500"
-              size={15}
-            />
+      <div className="container p-8 lg:hidden">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center gap-6">
+            <IoIosMenu size={30} />
+            <BsSearch size={20} />
           </div>
-          <div className="flex gap-4">
-            <div className="icon__wrapper">
-              <AiOutlineUser />
-            </div>
-            <div className="icon__wrapper relative">
-              <AiOutlineShoppingCart />
-              <Badge size="w-[25px] h-[25px]" />
-            </div>
+
+          <h1 className="text-4xl">Logo</h1>
+
+          <div className="flex gap-4 text-[30px]">
+            <AiOutlineUser />
+          </div>
+          <div className="relative cursor-pointer">
+            <AiOutlineShoppingCart size={30} />
+            <Badge size="w-[20px] h-[20px]" />
           </div>
         </div>
       </div>
@@ -34,4 +28,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default MobNav;
