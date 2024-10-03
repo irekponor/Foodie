030247130b1a2e1ad334a2c72584dfa1c83d0acc
emmanuel <emmanuelirekponor86@gmail.com>
2024-10-03 06:06:31 +0100

    components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -28,11 +28,16 @@ const data = [
 
 const Footer = () => {
   return (
-    <div className="container pt-16">
+    <div className="container pt-16 pb-7">
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
         {data.map((el) => (
           <FooterCard key={el.title} title={el.title} icon={el.icon} />
         ))}
+      </div>
+      <div className="border-t border-gray-300 mt-16">
+        <div className="container text-center text-gray-500 py-4">
+          &copy; Copyright @Emmanuel Irekponor 2024
+        </div>
       </div>
     </div>
   );
