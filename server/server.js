9549a5d9339deea0c17c9908307d300a04a42fd6
emmanuel <emmanuelirekponor86@gmail.com>
2024-10-09 01:44:15 +0100

    database

--- server/server.js
+++ server/server.js
@@ -64,20 +64,6 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-// login post configure
-app.post("/login", (req, res, next) => {
-  passport.authenticate("local", (err, user, info) => {
-    if (err) return next(err); // Handle error
-    if (!user) {
-      return res.status(401).json({ error: info.message });
-    }
-    req.logIn(user, (err) => {
-      if (err) return next(err);
-      return res.json({ message: "Login successful" });
-    });
-  })(req, res, next);
-});
-
 // register post configure
 app.post("/register", async (req, res) => {
   try {
