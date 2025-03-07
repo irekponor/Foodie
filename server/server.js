d80499004e3159e113978f69f498f3a72b985480
emmanuel <emmanuelirekponor86@gmail.com>
2025-03-07 01:36:00 +0100

    env variables

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  database: process.env.DB_PORT,
+  port: process.env.DB_PORT,
 });
 
 // Connect to the database
